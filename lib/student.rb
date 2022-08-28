d70e8f290936306ed010b9598b1355afae749a6e
otienosteve <steveotieno701@gmail.com>
2022-08-28 15:57:58 +0300

    will be done soon

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,75 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade,:id
+  def initialize(name,grade,id:nil)
+    @name=name
+    @grade=grade
+    @id=id
+  end 
+  def self.create_table
+  sql= <<-SQL
+  CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY ,
+    name TEXT,
+    grade TEXT
+  )
+  SQL
+  DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+  
+    DB[:conn].execute(sql)
+  end 
+  def save
+    if self.id
+       self.update
+    else
+    sql = <<-SQL
+      INSERT INTO students (name,grade)
+      VALUES (?,?)
+    SQL
+    
+    # insert tthe song
+    DB[:conn].execute(sql,self.name, self.grade)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    self
+  end
+  end
+  def self.create(name,grade)
+    student = Student.new(name,grade)
+    student.save
+    student
+  end
+  def self.new_from_db(row) 
+    self.new(row[0],row[1],row[2])
+  end
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+  SQL
+  
+  DB[:conn].execute(sql,name).each |value|do
+  sel.new_from_db(row)
+  end
 
-
-end
+  end
+  def update(id)
+    sql = <<-SQL
+    UPDATE songs SET name = ?, grade = ? WHERE id = ?
+  SQL
+  
+  DB[:conn].execute(sql,id).map do |row|
+    self.new_from_db(row)
+  end.first
+  end
+  
+  
+  end
